afa39f58dc3cc0c7dd909a66c4c4690dd08a6c55
BennyZ3 <bennyz3@hotmail.com>
2022-02-18 12:35:25 -0500

    added schema and base test data

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -5,12 +5,30 @@ CREATE DATABASE cta_dev;
 
 DROP TABLE IF EXISTS test;
 
-CREATE TABLE test (
+CREATE TABLE products (
     id SERIAL PRIMARY KEY, 
-    name TEXT REQUIRED,
+    name TEXT NOT NULL,
     description TEXT,
     price DECIMAL(10,2),
-    rating TINYINT check (rating>=0 and rating<=5),
+    rating SMALLINT,
+    check (rating>=0 and rating<=5),
     featured BOOLEAN,
-    image TEXT
+    image TEXT,
+    inventory INT
+);
+
+CREATE TABLE users (
+    username TEXT PRIMARY KEY,
+    email TEXT,
+    password TEXT,
+    admin BOOLEAN
+);
+
+CREATE TABLE transactions (
+    transact_id SERIAL PRIMARY KEY,
+    user_id TEXT,
+    product_id INT,
+    transaction_complete BOOLEAN,
+    FOREIGN KEY (user_id) REFERENCES users(username),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,21 +1,41 @@
 \c cta_dev;
 
-INSERT INTO test (name, description, price, rating, featured, image) VALUES
+INSERT INTO products (name, description, price, rating, featured, image, inventory) VALUES
 ('Nvidia GeForce RTX 3080', 
 'The GeForce RTX™ 3080 Ti and RTX 3080 graphics cards deliver the ultra performance that gamers crave, powered by Ampere—NVIDIA’s 2nd gen RTX architecture. They are built with enhanced RT Cores and Tensor Cores, new streaming multiprocessors, and superfast G6X memory for an amazing gaming experience.',
 699.00,
 5,
 true,
-'https://m.media-amazon.com/images/I/81kf3+5l2LS._AC_SX450_.jpg'),
+'https://m.media-amazon.com/images/I/81kf3+5l2LS._AC_SX450_.jpg',
+0),
 ('AMD Radeon™ RX 6800 XT',
 'The AMD Radeon™ RX 6800 XT graphics card, powered by AMD RDNA™ 2 architecture, featuring 72 powerful enhanced Compute Units, 128 MB of all new AMD Infinity Cache and 16GB of dedicated GDDR6 memory, is engineered to deliver ultra-high frame rates and serious 4K resolution gaming.',
 649.00,
 5,
 true,
-'https://drh1.img.digitalriver.com/DRHM/Storefront/Company/amd/images/product/thumbnail/663934-amd-radeon-rx-6800xt-box-100x100.png'),
+'https://drh1.img.digitalriver.com/DRHM/Storefront/Company/amd/images/product/thumbnail/663934-amd-radeon-rx-6800xt-box-100x100.png',
+3),
 ('NVIDIA GeForce RTX 3060 Ti 8GB GDDR6 PCI Express 4.0 Graphics Card - Steel and Black',
 'The GeForce RTX 3060 Ti lets you take on the latest games using the power of Ampere—NVIDIA’s 2nd generation RTX architecture. Discover incredible performance with enhanced Ray Tracing Cores and Tensor Cores, new streaming multiprocessors, and high-speed G6 memory.',
 399.99,
 4.7,
 true,
-'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6439/6439402cv11d.jpg;maxHeight=640;maxWidth=550');
+'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6439/6439402cv11d.jpg;maxHeight=640;maxWidth=550',
+5);
+
+INSERT INTO users (username, email, password, admin) VALUES
+('bennyz',
+'bennyzheng@pursuit.org',
+'qwertyuiop',
+true),
+('test',
+'test@test.org', 
+'test', 
+false);
+
+INSERT INTO transactions (user_id, product_id, transaction_complete) VALUES
+('bennyz', 1, true),
+('bennyz', 2, false),
+('test', 1, false),
+('test', 2, true),
+('test', 3, false);
